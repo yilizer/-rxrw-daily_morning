b56fdf173428be3a6fe660463e47cb6361861602
yilizer <114124284+yilizer@users.noreply.github.com>
2022-09-22 19:08:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import random
 
 today = datetime.now()
+date11=datetime.date.today ()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
@@ -49,6 +50,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":get_weather[0]},"temperature":{"value":temperature},"city":{"value":city},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"date":{"value":date11},"weather":{"value":get_weather()[0]},"max_temperature":{"value":get_weather()[2]},"min_temperature":{"value":get_weather()[3]},"city":{"value":city},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
