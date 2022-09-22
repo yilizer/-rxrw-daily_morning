c467c2b54f2867addc73289d6994b427ab611843
yilizer <114124284+yilizer@users.noreply.github.com>
2022-09-22 19:40:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather1():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-   return weather['weather'], math.floor(weather['temp']),weather['high'],weather['low']
+  return weather['weather'], math.floor(weather['temp']),weather['high'],weather['low']
 
 
 def get_weather():
